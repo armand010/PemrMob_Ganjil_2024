78c484f9eb32383dcee6bbcdad6d0a931133cd9f
armand010 <andikaputrasamawa@gmail.com>
2024-09-06 09:55:54 +0700

    perbaruan kode no 1

--- tugasWeek2no1.dart
+++ tugasWeek2no1.dart
@@ -1,18 +1,5 @@
 void main() {
-  // Variabel yang tidak bisa null
-  int nonNullVar = 10;
-
-  // Variabel yang bisa null
-  int? nullVar;
-
-  print('Non null vari: $nonNullVar');
-  print('Null variabel (before assignment): $nullVar');
-
-  // Menyusun null vari
-  nullVar = 20;
-  print('Null variabel (after assignment): $nullVar');
-
-  // nullVar bisa bernilai null tanpa error
-  nullVar = null;
-  print('Null variabel (after null assignment): $nullVar');
+  for (int i = 19; i > 9; i--) {
+    print('Nama saya adalah Fulan, sekarang berumur ${i - 1}');
+  }
 }
