509361fc62693e1991fcf6c5c47e0aab83a677b3
armand010 <andikaputrasamawa@gmail.com>
2024-09-13 06:40:13 +0700

    update tugas mobile

--- week-3/tugas.dart
+++ week-3/tugas.dart
@@ -1,22 +1,17 @@
 void main() {
-  // Nama lengkap dan NIM
-  String namaLengkap = 'Armand Maulana Andika Putra';
-  String nim = '2241720090';
-
   // Fungsi untuk memeriksa apakah sebuah bilangan adalah bilangan prima
   bool isPrime(int number) {
-    if (number <= 1) return false; // Bilangan 0 dan 1 bukan bilangan prima
+    if (number <= 1) return false;
     for (int i = 2; i <= number ~/ 2; i++) {
-      if (number % i == 0)
-        return false; // Bukan bilangan prima jika habis dibagi
+      if (number % i == 0) return false;
     }
-    return true; // Bilangan prima
+    return true;
   }
 
   // Looping untuk mencari bilangan prima dari 0 sampai 201
   for (int i = 0; i <= 201; i++) {
     if (isPrime(i)) {
-      print('$i $namaLengkap $nim');
+      print('$i Armand Maulana Andika Putra 2241720090');
     } else {
       print(i);
     }
