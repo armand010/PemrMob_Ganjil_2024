0c889deba5038b1a5381055a9172a6987f4a77f8
armand010 <andikaputrasamawa@gmail.com>
2024-10-11 10:51:39 +0700

    Membuat laporan

--- week-7.2/belanja/lib/main.dart
+++ week-7.2/belanja/lib/main.dart
@@ -1,13 +1,76 @@
-import 'package:belanja/pages/home_page.dart';
-import 'package:belanja/pages/item_page.dart';
+// import 'package:belanja/pages/home_page.dart';
+// import 'package:belanja/pages/item_page.dart';
+import 'package:card_loading/card_loading.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    initialRoute: '/',
-    routes: {
-      '/': (context) => HomePage(),
-      '/item': (context) => ItemPage(),
-    },
-  ));
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ExampleCardLoading(),
+    );
+  }
+}
+
+class ExampleCardLoading extends StatelessWidget {
+  const ExampleCardLoading({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My Space"),
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverPadding(
+            padding: const EdgeInsets.all(20),
+            sliver: SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        CardLoading(
+                          height: 30,
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                          width: 100,
+                          margin: EdgeInsets.only(bottom: 10),
+                        ),
+                        CardLoading(
+                          height: 100,
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                          margin: EdgeInsets.only(bottom: 10),
+                        ),
+                        CardLoading(
+                          height: 30,
+                          width: 200,
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                          margin: EdgeInsets.only(bottom: 10),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                childCount: 10,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+    // initialRoute: '/',
+    // routes: {
+    //   '/': (context) => HomePage(),
+    //   '/item': (context) => ItemPage(),
+    // },
--- week-8/flutter_plugin_pubdev/README.md
+++ week-8/flutter_plugin_pubdev/README.md
@@ -1,3 +1,29 @@
-# flutter_plugin_pubdev
+Nama : Armand Maulana Andika Putra
 
-A new Flutter project.
+
+Kelas : TI-3C
+
+
+NIM : 2241720090
+
+# Tugas Praktikum
+1. Output
+![Screenshot hello_world](image/Praktikum-js7.png)
+
+2. Jelaskan maksud dari langkah 2 pada praktikum tersebut!
+“flutter pub add auto_size_text” digunakan untuk menginstal plugin auto_size_text pada project
+
+3. Jelaskan maksud dari langkah 5 pada praktikum tersebut!
+- “final String text;” digunakan untuk mendeklarasikan sebuah variabel bernama text yang bertipe String.
+- “const RedTextWidget({Key? key, required this.text}) : super(key: key);” merupakan sebuah konstruktor untuk kelas RedTextWid
+
+4. Pada langkah 6 terdapat dua widget yang ditambahkan, jelaskan fungsi dan perbedaannya!
+- Kode ini membuat dua Container yang masing-masing memiliki warna latar belakang berbeda dan lebar yang berbeda.
+- Container pertama menampilkan teks menggunakan widget kustom RedTextWidget, yang memberikan warna merah pada teks.
+- Container kedua menampilkan teks yang sama tetapi menggunakan widget Text bawaan Flutter dengan default style dan tanpa perubahan khusus pada warna teks.
+
+5. Jelaskan maksud dari tiap parameter yang ada di dalam plugin auto_size_text berdasarkan tautan pada dokumentasi ini !
+- text: variabel yang menyimpan string yang akan ditampilkan. 
+- TextStyle: properti untuk mendefinisikan gaya teks.
+- maxLines: 2 membatasi teks untuk ditampilkan maksimal dalam 2 baris.
+- overflow: TextOverflow.ellipsis menentukan bagaimana teks akan dipotong jika melebihi batas maksimal jumlah baris
