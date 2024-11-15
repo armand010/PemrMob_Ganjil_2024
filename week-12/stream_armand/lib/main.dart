3c02d08acba9f13459aa5cbb40c218701a7d1adf
armand010 <andikaputrasamawa@gmail.com>
2024-11-15 09:21:33 +0700

    W12: Jawaban Soal 8

--- week-12/stream_armand/lib/main.dart
+++ week-12/stream_armand/lib/main.dart
@@ -35,6 +35,7 @@ class _StearmHomePageState extends State<StreamHomePage> {
   int lastNumber = 0;
   late StreamController numberStreamController;
   late NumberStream numberStream;
+  late StreamTransformer transformer;
 
   void changeColor() async {
     colorStream.getColors().listen((eventColor) {
@@ -46,9 +47,9 @@ class _StearmHomePageState extends State<StreamHomePage> {
 
   void addRandomNumber() {
     Random random = Random();
-    // int myNum = random.nextInt(10);
-    // numberStream.addNumberToSink(myNum);
-    numberStream.addError();
+    int myNum = random.nextInt(10);
+    numberStream.addNumberToSink(myNum);
+    // numberStream.addError();
   }
 
   @override
@@ -56,7 +57,29 @@ class _StearmHomePageState extends State<StreamHomePage> {
     numberStream = NumberStream();
     numberStreamController = numberStream.controller;
     Stream stream = numberStreamController.stream;
-    stream.listen((event) {
+
+    // Listener 1
+    // stream.listen((event) {
+    //   setState(() {
+    //     lastNumber = event;
+    //   });
+    // }).onError((error) {
+    //   setState(() {
+    //     lastNumber = -1;
+    //   });
+    // });
+
+    //Listener 2
+    transformer = StreamTransformer<int, int>.fromHandlers(
+        handleData: (value, sink) {
+          sink.add(value * 10);
+        },
+        handleError: (error, trace, sink) {
+          sink.add(-1);
+        },
+        handleDone: (sink) => sink.close());
+
+    stream.transform(transformer).listen((event) {
       setState(() {
         lastNumber = event;
       });
@@ -65,6 +88,7 @@ class _StearmHomePageState extends State<StreamHomePage> {
         lastNumber = -1;
       });
     });
+
     super.initState();
   }
 
