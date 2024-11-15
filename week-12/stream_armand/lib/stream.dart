eb73f579f6c929501dad15f3019fe6a21b2edaf6
armand010 <andikaputrasamawa@gmail.com>
2024-11-15 07:41:33 +0700

    W13: Jawaban Soal 2

---
+++ week-12/stream_armand/lib/stream.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorsStream {
+  final List<Color> colors = [
+    Colors.red,
+    Colors.yellow,
+    Colors.green,
+    Colors.lightBlue,
+    Colors.purple,
+  ];
+}
